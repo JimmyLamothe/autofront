14cf9b2e2c6f4740f8e9aaf3aa4c2535645e1a51
JimmyLamothe <jm_laprise@hotmail.com>
2020-03-21 18:44:12 -0400

    Fixed display.

--- decorators.py
+++ decorators.py
@@ -2,9 +2,22 @@ import contextlib, datetime
 
 def redirect_print(func, *args):
     def wrapper(*args):
-        with open('test.txt', 'a') as out:
+        with open('display.txt', 'a') as out:
             with contextlib.redirect_stdout(out):
                 print(datetime.datetime.now())
                 print(func.__name__)
                 return func(*args)
     return wrapper
+
+
+
+#Using tempfile - Doesn't work
+"""
+def redirect_print_temp(func, *args):
+    def wrapper(*args):
+        with contextlib.redirect_stdout(resources.display):
+                print(datetime.datetime.now())
+                print(func.__name__)
+                return func(*args)
+    return wrapper
+"""
