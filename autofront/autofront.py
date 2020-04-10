cf1f03ce06aaadf611df270d0c0f02f884b6238e
JimmyLamothe <jm_laprise@hotmail.com>
2020-04-10 19:06:24 -0400

    Refactored code. Combined tests on one page.

--- autofront/autofront.py
+++ autofront/autofront.py
@@ -1,77 +1,79 @@
-import sys, subprocess
+import sys
 from flask import Flask, redirect, url_for, render_template, request
-from autofront.decorators import redirect_print, display_path
-from autofront.utilities import parse_args, get_function
+#from autofront.decorators import redirect_print, display_path
+from autofront.utilities import redirect_print, clear_display, get_display
+from autofront.utilities import initialize, run_script
+from autofront.utilities import get_function, get_args
 
 print('Development version active')
 
-app = Flask(__name__)
-
-def clear_display():
-    with open(display_path + '/display.txt', 'w') as out:
-        pass
-
-clear_display()
+app = initialize(__name__)
 
 func_dicts = [] #List of link address and link name for each function
-
 
 #TO DO: Make multiple page formats - (choose layout, change title, etc)
 def functions():
     if request.method =='POST':
         func_name = list(request.form.keys())[0]
         function = get_function(func_name, func_dicts)
-        arg_string = list(request.form.values())[0]
-        all_args = parse_args(arg_string)
+        all_args = get_args(request, func_name, func_dicts)
         args = all_args[0]
         kwargs = all_args[1]
         clear_display()
         @redirect_print
-        def new_function():
+        def wrapper():
             function(*args, **kwargs)
-        new_function.__name__ = function.__name__
-        new_function()
+        wrapper.__name__ = function.__name__
+        wrapper()
         return redirect(url_for('functions'))
-    with open(display_path + '/display.txt', 'r') as filepath:
-        display = filepath.read()
-        display = display.split('\n')
-    return render_template('functions.html', title = 'functions', display = display,
-                           func_dicts = func_dicts)
+    display = get_display()
+    return render_template('functions.html', title = 'functions',
+                           display = display, func_dicts = func_dicts)
 
 app.add_url_rule('/', 'functions', functions, methods=['GET', 'POST'])
 
-def run_script(script, *args):
-    script_path = './' + script
-    command_list = list(args)
-    command_list.insert(0, script_path)
-    command_list.insert(0, 'python')
-    print(command_list)
-    def new_function():
-        with open(display_path + '/display.txt', 'a') as out:
-            subprocess.run(command_list, stdout = out)
-    new_function.__name__ = script
-    return new_function
 
-def create_route(function, *args, link = None, title = None, **kwargs):
+
+def create_route(function, *args, link = None, title = None,
+                 live = False, script = False, mixed_args = False,
+                 **kwargs):
+    if script == 'DONE':
+        pass
+    elif script:
+        create_route(run_script(function, *args), *args, link = link,
+                     title = title, live = live, script = 'DONE',
+                     mixed_args = mixed_args, **kwargs)
+        return
+    func_name = function.__name__
+    print('args for ' + func_name + ': ' + str(args))
+    print('kwargs for ' + func_name + ': ' + str(kwargs))
     if not link:
-        link = function.__name__
+        link = func_name
     if not title:
-        title = function.__name__
+        title = func_name
     def new_route():
         clear_display()
-        @redirect_print
-        def new_route():
-            function(*args, **kwargs)
-        new_route.__name__ = function.__name__
-        new_route()
+        if mixed_args:
+            pass
+        elif script == 'DONE':
+            wrapper = function
+        else:
+            @redirect_print
+            def wrapper():
+                function(*args, **kwargs)
+        wrapper.__name__ = func_name
+        wrapper()
         return redirect(url_for('functions'))
     app.add_url_rule('/' + link, link, new_route)
-    func_dicts.append({'func': function, 'link':link,
-                       'title':title, 'live':False})
-
-def create_live_route(function, *args, link = None, title = None, **kwargs):
-    create_route(function, *args, link = link, title = title, **kwargs)
-    func_dicts[-1]['live'] = True
-
+    func_dicts.append({'func': function,
+                       'link':link,
+                       'title':title,
+                       'live':live,
+                       'script':script,
+                       'mixed_args':mixed_args})
+    if mixed_args:
+        func_dicts[-1]['args'] = [*args]
+        func_dicts[-1]['kwargs'] = {**kwargs}
+    
 def run(host='0.0.0.0', port = 5000):
     app.run(host = host, port = port)
--- autofront/utilities.py
+++ autofront/utilities.py
@@ -1,14 +1,82 @@
-import string
+import contextlib, datetime, sys, pathlib, string, subprocess
+from flask import Flask
 
-def get_function(func_title, func_dicts):
-    functions = [func_dict for func_dict in func_dicts
+def initialize(name):
+    clear_display()
+    app = Flask(name)
+    return app
+
+display_path = str(pathlib.Path(__file__).parent)
+
+def clear_display():
+    with open(display_path + '/display.txt', 'w') as out:
+        pass
+
+def get_display():
+    with open(display_path + '/display.txt', 'r') as filepath:
+        display = filepath.read()
+        display = display.split('\n')
+    return display
+
+"""
+Used as a decorator to print to a file for display
+in the browser instead of to the console.
+If using multiprocessing, it's essential to add "sys.stdout.flush()"
+after the print calls in your child process, otherwise the file
+will only be written on program exit. Otherwise,
+your original function should not need any modification.
+"""
+
+def redirect_print(func, *args):
+    def wrapper(*args):
+        with open(display_path + '/display.txt', 'a') as out:
+            print(display_path)
+            with contextlib.redirect_stdout(out):
+                #print(datetime.datetime.now()) #Uncomment for debugging
+                #print(func.__name__) #Uncomment for debugging
+                wrapped_func = func(*args)
+                return wrapped_func
+    return wrapper
+
+
+def run_script(script, *args):
+    script_path = './' + script
+    command_list = list(args)
+    command_list.insert(0, script_path)
+    command_list.insert(0, 'python')
+    print(command_list)
+    def new_function():
+        with open(display_path + '/display.txt', 'a') as out:
+            subprocess.run(command_list, stdout = out)
+    new_function.__name__ = script
+    return new_function
+
+
+def get_func_dict(func_title, func_dicts):
+    func_dict = [func_dict for func_dict in func_dicts
                  if func_dict['title'] == func_title]
-    if len(functions) > 1:
+    if len(func_dict) > 1:
         raise Exception('Cannot use same title for your functions')
     else:
-        func_dict = functions[0]
-        function = func_dict['func']
-        return function
+        func_dict = func_dict[0]
+    return func_dict
+
+def get_fixed_args(func_title, func_dicts):
+    func_dict = get_func_dict(func_title, func_dicts)
+    all_fixed_args = []
+    fixed_args = []
+    fixed_kwargs = []
+    if func_dict['mixed_args']:
+        fixed_args = func_dict['args']
+        fixed_kwargs = func_dict['kwargs']
+    if fixed_args or fixed_kwargs:
+        all_fixed_args = [fixed_args, fixed_kwargs]
+    return all_fixed_args
+
+def get_function(func_title, func_dicts):
+    func_dict = get_func_dict(func_title, func_dicts)
+    function = func_dict['func']
+    return function
 
 def strip_spaces(input):
     return input.replace(' ', '')
@@ -36,4 +104,23 @@ def parse_args(arg_string):
     
     kwargs = parse_kwargs(kwargs)
     all_args = [args, kwargs]
+    return all_args
+
+def get_args(request, func_name, func_dicts):
+    arg_string = list(request.form.values())[0]
+    all_args = parse_args(arg_string)
+    print(all_args)
+    args = all_args[0]
+    print(args)
+    kwargs = all_args[1]
+    print(kwargs)
+    fixed_args = get_fixed_args(func_name, func_dicts)
+    print(fixed_args)
+    if fixed_args:
+        args = fixed_args[0] + args
+        print(args)
+        kwargs = kwargs.update(fixed_args[1])
+        print(kwargs)
+    all_args = [args, kwargs]
+    print(all_args)
     return all_args
